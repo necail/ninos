117fcd4a3dad7e39925d3f16524271ef01692bf1
Jose Antonio Gonzalez <joseantonio@datamind.cl>
2016-10-22 19:41:48 -0700

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2'
+
 
 import { AppComponent } from './app.component';
+import { ActionComponent } from './action/action.component';
+import { ActionListComponent } from './action/action-list.component';
+import { AdminComponent } from './admin/admin.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDk7tqiop3qupxCUDtJ6YMEE0e2SvV3x00",
+  authDomain: "fiery-inferno-6665.firebaseapp.com",
+  databaseURL: "https://fiery-inferno-6665.firebaseio.com",
+  storageBucket: "fiery-inferno-6665.appspot.com",
+  messagingSenderId: "1069801040899"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActionComponent,
+    ActionListComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'admin', component: AdminComponent },
+      { path: '', component: ActionListComponent }
+    ]),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
